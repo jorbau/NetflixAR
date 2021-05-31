e0f5f8bab8284a45da837abace887fb448d64102
Roberto-Uni <80832973+Roberto-Uni@users.noreply.github.com>
2021-06-01 00:09:45 +0200

    Codigo final

--- ImageClassifierSIFT.py
+++ ImageClassifierSIFT.py
@@ -1,141 +1,160 @@
 import cv2
 import numpy as np
 import os
-from imutils.convenience import resize
 import time
 import json
 from itertools import compress
 from collections import Counter
 import sys
+from matplotlib import pyplot as plt
+import statistics as stats
 
+def load_file(cl):
+    if cl[-1] == 'y':
+        des = np.loadtxt(cl).astype('float32')
+        return des, "d"
 
-def findid(img, desList, thres=25):
-    kp2, des2 = sift.detectAndCompute(img, None)
-    bf = cv2.BFMatcher()
-    matchList = []
-    finalVal = -1
-    try:
-        for des in desList:
-            matches = bf.knnMatch(des, des2, k=2)
-            good = []
-            for m, n in matches:
-                if m.distance < 0.75*n.distance:
-                    good.append(m)
-            matchList.append(len(good))
-            #print(len(good))
-    except:
-        #print("Reventó")
-        pass
-    #print(matchList)
-    if len(matchList) != 0:
-        if max(matchList) > thres:
-            finalVal = matchList.index(max(matchList))
-            #print(finalVal)
-    return finalVal
-
-
-def findDes(images):
-    desList = []
-    for img in images:
-        kp2, des = sift.detectAndCompute(img, None)
-        desList.append(des)
-    return desList
-
-
-def pyramid(image, scale=1.5, minSize=(30, 30)):
-    # yield the original image
-    yield image
-
-    # keep looping over the pyramid
-    while True:
-        # compute the new dimensions of the image and resize it
-        w = int(image.shape[1] / scale)
-        image = resize(image, width=w)
-
-        # if the resized image does not meet the supplied minimum
-        # size, then stop constructing the pyramid
-        if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
-            break
-
-        # yield the next image in the pyramid
-        yield image
-
-
-def sliding_window(image, stepSize, windowSize):
-    # slide a window across the image
-    for y in range(0, image.shape[0], 100):
-        for x in range(0, image.shape[1], 150):
-            # yield the current window
-            yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
-
-def test():
-
-    sift = cv2.xfeatures2d.SIFT_create()
-    bf = cv2.BFMatcher()
-    img1  = cv2.imread("Im2.jpeg")
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    img2 = cv2.imread("245271.png")
-    kp2, des2 = sift.detectAndCompute(img2, None)
-    matches = bf.knnMatch(des1, des2, k=2)
+    else:
+        kps = []
+        lines = [line.strip() for line in open(cl)]
+        for line in lines:
+            list = line.split(',')
+            kp = cv2.KeyPoint(x=float(list[0]), y=float(list[1]), _size=float(list[2]), _angle=float(list[3]),
+                        _response=float(list[4]), _octave=int(list[5]), _class_id=int(list[6]))
+            kps.append(kp)
+        return kps, 'k'
     
 
-    good = []
-    for m,n in matches:
-        if m.distance < 0.75*n.distance:
-            good.append([m])
+def get_coor(text, coor):
+    font = cv2.FONT_HERSHEY_COMPLEX
+    textsize = cv2.getTextSize(text, font, 2, 4)[0]
+    
+    textX = coor[0] - textsize[0]//2
+    textY = coor[1] + textsize[1]//2
+    return (textX, textY)
 
-
-
-    img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
-    cv2.imshow('img3', img3)
-
-    cv2.waitKey(0)
-
-#test()
-#sys.exit()
-
+time_i = time.time()
+print("Cargando...")
 with open('Netflix_data.json') as f:
   data = json.load(f)
-path = 'DBAnime'
-sift = cv2.xfeatures2d.SIFT_create()
+path = 'features/'
 # Import Images
 images = []
 className = []
-
+des_list = []
+kp_list = []
 myList = os.listdir(path)
-print('Total Classes Detectted: ', len(myList))
+i = -1
 for cl in myList:
-    imgCur = cv2.imread(f'{path}/{cl}')
-    images.append(imgCur)
-    className.append(os.path.splitext(cl)[0])
-# print(className)
+    if "_1.npy" in cl:
+        i += 1
+        images.append([cl[:cl.index("_")], [],[]])
+    des,t = load_file(path+cl)
+    if t == 'd':
+        images[i][1].append(des)
+    else:
+        images[i][2].append(des)
+print('Total Classes Detectted: ', len(images))
+FLANN_INDEX_KDTREE = 1
+MIN_MATCH_COUNT = 15
+sift = cv2.xfeatures2d.SIFT_create(nfeatures=100000)
 
-desList = findDes(images)
-print(len(desList))
-
-
-# load the image and define the window width and height
-image = cv2.imread("Netflix1.jpg")
-(winW, winH) = (300, 200)
-
-ants = []
-# loop over the image pyramid
-for resized in pyramid(image, scale=1.5):
-    # loop over the sliding window for each layer of the pyramid
-    for (x, y, window) in sliding_window(resized, stepSize = 150, windowSize=(winW, winH)):
-        # if the window does not meet our desired window size, ignore it
-        #if window.shape[0] != winH or window.shape[1] != winW:
-        #    continue
-        clone = resized.copy()
-        id = findid(window, desList)
-        if id != -1:
-            for element in data:
-                if str(element['id']) == className[id] and element['title'] not in ants:
-                    ants.append(element['title'])
-                    cv2.putText(image, element['title'], (int(x), int(y + winH/2)), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-                    cv2.putText(image, element['title'], (int(x), int(y + winH/2)), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 1)
+img2 = cv2.imread("rob1.jpeg")
+Im_g = "resultado23.png"
+print("Iniciando...")
+kp2, des2 = sift.detectAndCompute(img2, None)
+area_list = []
+dst_list = []
+name_list = []
+for cl, deslist, kplist in images:
+    good_list = []
+    kp1_list = []
+    for kp1, des1 in zip(kplist,deslist):
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
+        bf = cv2.FlannBasedMatcher(index_params, search_params)
+        matches = bf.knnMatch(des1,des2,k=2)
+        good = []
+        for m,n in matches:
+            if m.distance < 0.7*n.distance:
+                good.append(m)
+        if len(good)>MIN_MATCH_COUNT:
+            good_list.append(good)
+            kp1_list.append(kp1)
+            
+    if len(good_list)>0:
+        good = good_list[0]
+        kp1 = kp1_list[0]
+        for n in range(len(good_list)):
+            if len(good_list[n])>len(good):
+                good = good_list[n]
+                kp1 = kp1_list[n]
+                
+        try:
+            src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+            dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            matchesMask = mask.ravel().tolist()
+            h,w,d = (192, 341, 3)
+            pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+            if len(cv2.convexHull(pts, returnPoints = False)) != 4: continue
+            dst = cv2.perspectiveTransform(pts,M)
+            area_list.append((h-1)*(w-1))
+            dst_list.append(dst)
+            name_list.append(cl)
+        except:
+            print(cl)
+            
+area_list.sort()
+mediana = stats.median(area_list)
+overlay = img2.copy()
+score_info = []
+for i in range(len(area_list)-1):
+    if area_list[i]<=mediana+10000 and area_list[i]>=mediana-10000:
+        for dic in data:
+            if str(dic["id"]) == name_list[i]:
+                long = max(abs(dst_list[i][0][0][0] - dst_list[i][1][0][0]), 
+                           abs(dst_list[i][0][0][0] - dst_list[i][2][0][0]),
+                           abs(dst_list[i][0][0][0] - dst_list[i][3][0][0]),
+                           abs(dst_list[i][1][0][0] - dst_list[i][2][0][0]),
+                           abs(dst_list[i][1][0][0] - dst_list[i][3][0][0]),
+                           abs(dst_list[i][2][0][0] - dst_list[i][3][0][0]))
+                if long>0:
+                    text_len = len(dic['title'])
+                    text_size = cv2.getTextSize(dic['title'], cv2.FONT_HERSHEY_COMPLEX, 1, 2)[0][0]
+                    caracter = text_size/text_len
+                    cv2.putText(img2, dic['title'][:int(long/caracter)], tuple(dst_list[i][0][0]), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                    cv2.putText(img2, dic['title'][:int(long/caracter)], tuple(dst_list[i][0][0]), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 1)
+                else:
                     break
-        #cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
-        #cv2.imshow('resultado2', window)
-        #cv2.waitKey(1)
-cv2.imwrite("resultado.png", image)
+                scr_pos = [0, 0]
+                for x in dst_list[i]:
+                    scr_pos[0] += int(x[0][0]/4)
+                    scr_pos[1] += int(x[0][1]/4)
+                
+                color = (255, 255, 255)
+                if 'score' in dic:
+                    score_info.append([str(dic['score']), tuple(scr_pos)])
+                    if dic['score'] < 5:
+                        color = (0,0,255)
+                    elif 7>dic['score'] >=5:
+                        color = (0,131,255)
+                    elif 9>dic['score'] >=7:
+                        color = (0,255,255)
+                    else:
+                        color = (0,179,55)
+                else:
+                    score_info.append(["S.N.", tuple(scr_pos)])
+                cv2.fillPoly(overlay, [np.int32(dst_list[i])], color)
+                break
+        
+
+alpha = 0.6
+img2 = cv2.addWeighted(overlay, alpha, img2, 1, 0)
+for score, pos in score_info:
+    pos = get_coor(score, pos)
+    cv2.putText(img2, score, pos, cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 4)
+    cv2.putText(img2, score, pos, cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 0), 2)
+
+print(time.time()-time_i)
+cv2.imwrite(Im_g,img2)
